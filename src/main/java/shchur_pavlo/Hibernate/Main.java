80ca5cb82cb811208000ee425c4c28951de5eb53
Pavlo Shchur <pavloshchur@gmail.com>
2017-08-19 22:24:16 +0300

    Hibernate

--- src/main/java/shchur_pavlo/Hibernate/Main.java
+++ src/main/java/shchur_pavlo/Hibernate/Main.java
@@ -1,13 +1,31 @@
 package shchur_pavlo.Hibernate;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
+import java.util.logging.Logger;
+
 public class Main {
+
+	private static Logger logger = Logger.getLogger(Main.class.getName());
 
 	public static void main(String[] args) {
 		
 		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-		
+		Session session = sessionFactory.openSession();
+
+		try {
+			session.beginTransaction();
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			session.getTransaction().rollback();
+			e.printStackTrace();
+			logger.info(e.toString());
+		} finally {
+			session.close();
+			sessionFactory.close();
+		}
+
 	}
 
 }
